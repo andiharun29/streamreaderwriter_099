59aaa9d9ba65a7941daa58ae4e1840ec4559807d
andiharun29 <andiharunalrasyid14@gmail.com>
2023-06-21 11:03:22 +0700

    menambahkan file dan menunjuk ke sebuah file

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -7,3 +7,12 @@ int main() {
 	string baris;
 	string namafile;
 
+	cout << "masukkan nama file : ";
+	cin >> namafile;
+
+	//membuka file dalam mode menuis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open(namafile + ".txt", ios::out);
+
+	
